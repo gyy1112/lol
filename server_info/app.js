a43cc2deab8037a3a0c5dd9ac721420fdc368b56
unknown <872541133@qq.com>
2019-06-04 09:11:40 +0800

    配置服务器

--- server_info/app.js
+++ server_info/app.js
@@ -5,7 +5,7 @@ const userRouter = require('./routes/user_login.js')
 var server = express()
 server.listen('8080',()=>console.log('服务器启动成功'))
 //托管静态资源到public
-server.use(express.static('public'))
+server.use(express.static('../previous_info/public'))
 server.use(bodyParser.urlencoded({
    extended:false
 }))
--- server_info/routes/user_login.js
+++ server_info/routes/user_login.js
@@ -2,4 +2,28 @@ const express = require('express')
 const pool = require('../pool.js')
 var router = express.Router()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router
