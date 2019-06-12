2a97439c427eba15d7d71439875d0aeabe03a4a0
unknown <872541133@qq.com>
2019-06-12 20:59:58 +0800

    登录验证完成

---
+++ previous_info/js/index.js
@@ -0,0 +1,28 @@
+(()=>{
+  var timer;
+  var index=0
+  var width=$(".carousel ul li").eq(0).width()
+  var lis=document.querySelectorAll(".carousel>ul>li")
+  $("#carousel-lists span").mouseover(function(e){
+    $(this).addClass("selected").siblings(".selected").removeClass("selected");
+    var index=$(this).index()
+    $(".carousel ul li").eq(index).stop().fadeIn(500).siblings().stop().fadeOut(500);
+  }) 
+  timer=setInterval(function(){
+    index++;
+    if(index==5){
+      index=0
+    }
+    $("#carousel-lists span").eq(index).addClass("selected").siblings().removeClass("selected");
+    $(".carousel ul li").eq(index).stop().fadeIn().siblings().stop().fadeOut();
+  },5000);
+
+  function tablist(selectors){
+    $(selectors).mouseover(function(e){
+      $(this).addClass("selected").siblings(".selected").removeClass("selected")
+    })
+  } 
+  tablist(".tab-top ul li")   
+  tablist(".m-act-title ul li")   
+  tablist(".m-video-title ul li")   
+})()
