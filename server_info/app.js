6e153c273d9086bba51c535614f0ce32515ff18d
unknown <872541133@qq.com>
2019-06-20 18:07:05 +0800

    跨域

--- server_info/app.js
+++ server_info/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const userRouter = require('./routes/router.js')
+const cors = require("cors")
 //创建web服务器
 var server = express()
 server.listen('8080',()=>console.log('服务器启动成功'))
 //托管静态资源到public
 server.use(express.static('../previous_info/public'))
+server.use(cors({origin:"http://127.0.0.1:5500"}))
 server.use(bodyParser.urlencoded({
    extended:false
 }))
